da847a8533357fb89baa9ec96b8e673433c62c03
Giedrius <giedriusj1@gmail.com>
2023-09-10 18:34:12 +0100

    Do everything from a thread with increased stack size

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,37 @@ mod lex;
 mod sexp;
 
 fn main() {
-    // read parameter passed to the program
-    let args: Vec<String> = std::env::args().collect();
+    // time how long it takes to run the program
+    let start = std::time::Instant::now();
 
-    let filename: String = if args.len() < 2 { "./test.g1".to_string() } else { args[1].clone() };
+    let child = std::thread::Builder::new()
+        .stack_size(512 * 1024 * 1024)
+        .spawn(move || {
+            // read parameter passed to the program
+            let args: Vec<String> = std::env::args().collect();
 
-    let text = std::fs::read_to_string(filename)
-        .unwrap()
-        .lines()
-        .filter(|&line| !line.trim().starts_with(";;"))
-        .collect::<Vec<_>>()
-        .join("\n");
+            let filename: String = if args.len() < 2 { "./test.g1".to_string() } else { args[1].clone() };
 
-    let tokens: Vec<lex::Token> = lex::extract_tokens(text);
-    println!("tokens {tokens:#?}");
+            let text = std::fs::read_to_string(filename)
+                .unwrap()
+                .lines()
+                .filter(|&line| !line.trim().starts_with(";;"))
+                .collect::<Vec<_>>()
+                .join("\n");
 
-    let sexp = sexp::create_sexp(tokens);
-    println!("sexp {sexp:#?}");
+            let tokens: Vec<lex::Token> = lex::extract_tokens(text);
+            println!("tokens {tokens:#?}");
 
-    let eval = eval::eval_sexp(sexp);
-    println!("eval {eval:#?}");
+            let sexp = sexp::create_sexp(tokens);
+            println!("sexp {sexp:#?}");
+
+            let eval = eval::eval_sexp(sexp);
+            println!("eval {eval:#?}");
+        })
+        .unwrap();
+
+    child.join().unwrap();
+
+    let end = std::time::Instant::now();
+    println!("execution time: {:?}", end - start);
 }
