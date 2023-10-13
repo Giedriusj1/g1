5368706396c66b40d45d3f784974ff96b3939316
Giedrius <giedriusj1@gmail.com>
2023-10-13 23:24:02 +0100

    improve tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,18 @@
-#! /bin/sh
+#!/bin/sh
 
 echo "Running tests..."
 
 # enumerate all files beginning with test-*
 for test_file in test-*; do
     echo "Running $test_file..."
-    cargo run ./$test_file
+
+    # capture the output of cargo run
+    output=$(cargo run ./$test_file 2>/dev/null)
+
+    # check if the output contains the test string
+    if echo "$output" | grep -q "eval to: t"; then
+        echo "Test passed!"
+    else
+        echo "Test failed: Expected to find '$test_string', but got '$output'"
+    fi
 done
