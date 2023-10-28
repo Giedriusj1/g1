5452020ca211a66cb802769f9f1b79a3cf680126
Giedrius <giedriusj1@gmail.com>
2023-10-28 14:44:04 +0100

    add some colours to test runner sh

--- run-tests.sh
+++ run-tests.sh
@@ -2,17 +2,22 @@
 
 echo "Running tests..."
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # enumerate all files beginning with test-*
 for test_file in test-*; do
-    echo "Running $test_file..."
+
+    # echo "Running $test_file..."
 
     # capture the output of cargo run
     output=$(cargo run ./$test_file 2>/dev/null)
 
     # check if the output contains the test string
     if echo "$output" | grep -q "eval to: t"; then
-        echo "Test passed!"
+        echo -e "${GREEN}[OK]    ${NC} $test_file"
     else
-        echo "Test failed: Expected to find '$test_string', but got '$output'"
+        echo -e "${RED}[FAILED]${NC} $test_file: Expected to find '$test_string', but got '$output'"
     fi
 done
