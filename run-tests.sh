5c50b35e09de34867f9313351834112f57408189
Giedrius <giedriusj1@gmail.com>
2023-11-03 10:43:37 +0000

    run tests concurrently

--- run-tests.sh
+++ run-tests.sh
@@ -6,18 +6,21 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# enumerate all files beginning with test-*
-for test_file in test-*; do
-
-    # echo "Running $test_file..."
-
-    # capture the output of cargo run
+# Function to run tests and print the result
+run_test() {
+    test_file="$1"
     output=$(cargo run ./$test_file 2>/dev/null)
-
-    # check if the output contains the test string
     if echo "$output" | grep -q "eval to: t"; then
         echo -e "${GREEN}[OK]    ${NC} $test_file"
     else
-        echo -e "${RED}[FAILED]${NC} $test_file: Expected to find '$test_string', but got '$output'"
+        echo -e "${RED}[FAILED]${NC} $test_file: Expected to find 'eval to: t', but got '$output'"
     fi
+}
+
+# Find all files beginning with test-* and run tests concurrently
+for test_file in test-*; do
+    run_test "$test_file" &
 done
+
+# Wait for all background processes to finish
+wait
