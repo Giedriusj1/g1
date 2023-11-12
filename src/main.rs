3c219b99f60357ce6357c433e273d93ff59b51f1
Giedrius <giedriusj1@gmail.com>
2023-11-12 18:38:22 +0000

    move test related intrinsics into test-harness.g1

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,10 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     let filename = if args.len() < 2 { "./test.g1".to_string() } else { args[1].clone() };
+    let sexp = create_sexp_from_file(filename);
 
     let intrinsics_sexp = create_sexp_from_file("./built-in/intrinsics.g1".to_string());
-
-    let sexp = create_sexp_from_file(filename);
+    let test_harness_sexp = create_sexp_from_file("./built-in/test-harness.g1".to_string());
 
     // Create eval state struct
     let mut eval_state = eval::EvalState::new();
@@ -43,6 +43,8 @@ fn main() {
     // Evaluate intrinsics
     // TODO: check results
     eval::eval_sexp(&intrinsics_sexp, &mut eval_state);
+
+    eval::eval_sexp(&test_harness_sexp, &mut eval_state);
 
     let eval = eval::eval_sexp(&sexp, &mut eval_state);
 
